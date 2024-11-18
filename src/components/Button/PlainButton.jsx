69785fbcc6e6baa0ad0fa0078bd000ca81c1e806
vidushisaxen <vidushisaxenabdn786@gmail.com>
2024-11-18 14:11:31 +0530

    update-[vidushi]
    Plain button animation

--- src/components/Button/PlainButton.jsx
+++ src/components/Button/PlainButton.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import styles from "./styles.module.css";
+import Link from 'next/link';
 
-const PlainButton = ({ text, link }) => {
+const TrialButton = ({text,link}) => {
   return (
-    <Link href={link}>
-      <div className="border-[1.5px] border-[#134BD6] text-center flex items-center justify-center px-[1.5vw] py-[0.9vw] rounded-[40px] min-w-[7vw] group hover:bg-[#134BD6] hover:text-white duration-300 transition-all ease cursor-pointer">
-        <p className="text-[0.97vw] font-light group-hover:text-white ">{text}</p>
+    <Link href={link}
+    >
+    <div className="w-fit h-fit flex items-center justify-center text-center   min-w-[9vw]">
+      <div
+        className={`${styles.buttonPlain} relative overflow-hidden w-full h-full p-0 border-[1.5px] border-[#134BD6] rounded-[40px] text-[#111111] text-[0.97vw] font-light px-[2vw] py-[1.5vw]` }
+        data-text={`${text}`}
+      >
+        <span className='text-[0.97vw] font-light '>{`${text}`}</span>
       </div>
+    </div>
     </Link>
-  )
-}
+  );
+};
 
-export default PlainButton
+export default TrialButton;
