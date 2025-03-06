8e57690b93e1831ed66cbc56fd6a031ecfbfdd23
Vidushi Saxena <vidushisaxenabdn786@gmail.com>
2025-03-06 14:50:08 +0530

    update
    industry page content

--- src/pages/industry.js
+++ src/pages/industry.js
@@ -14,16 +14,16 @@ const industry = () => {
   return (
     <>
       <Layout>
-        <Hero title1={"Our"} title2={"Industries"} para={"SEO is vastly interdisciplinary and it requires skills ranging from coding to linguistics to production. It includes services such as Influencer Marketing and Organic Social Media Management."} img={"/assets/images/industry/industry-hero.png"} />
+        <Hero title1={"Our"} title2={"Industries"} para={"Our versatile digital strategies span diverse industries. We harness data, creativity, and insights to craft bespoke solutions that empower brands and drive growth in an ever-evolving digital world."} img={"/assets/images/industry/industry-hero.png"} />
         <main>
           <section className='w-screen h-full py-[5vw]'>
             <div className='container-lg text-center flex items-center justify-center  h-full'>
               <div className='w-[75%] text-center'>
                 <p data-para-anim className='content py-[1vw]'>
-                  At HiveMinds, we tackle your most challenging business problems with our specialised digital marketing approach - integrating strategic expertise, cutting-edge technology, and data-driven insights to craft campaigns that elevate your brand and drive sustainable growth.
-                </p>
-                <p data-para-anim className='content py-[1vw]'>
-                  At HiveMinds, we tackle your most challenging business problems with our specialised digital marketing approach - integrating strategic expertise, cutting-edge technology.
+                  At HiveMinds, we provide comprehensive digital marketing services across industries. We tailor
+                  strategies to meet the unique needs of each sector, blending deep market research, data-driven
+                  insights, and creative innovation. Our expertise spans B2B, D2C, fashion, BFSI, health, travel,
+                  and more, ensuring sustainable growth and measurable success every single day.
                 </p>
               </div>
 
@@ -39,19 +39,13 @@ const industry = () => {
                   </div>
                   <div className='w-[50%]'>
                     <h2 className='text-[2.8vw] font-medium headinganim'>
-                     {item.title1}
+                      {item.title1}
                       <span className='text-primary'> {item.title2} </span>
                     </h2>
-                   
+
                     <p className='content tracking-wide para py-[1vw]' dangerouslySetInnerHTML={{ __html: item.para }} />
-                      <ul className='list-disc flex flex-wrap gap-x-[2vw] w-[65%] px-[1vw] py-[2vw]'>
-                      {item.list.map((item,index)=>(
-                         <li key={index}  className="content tracking-wide para w-fit">{item}</li>
-                      ))}
-                      </ul>
-                      
-                    </div>
                   </div>
+                </div>
               ))}
             </div>
 
@@ -67,105 +61,67 @@ export default industry
 const industries = [
   {
     img: "/assets/images/industry/industry-listing.png",
-    title1: " E-",
-    title2: "Commerce",
-    para: "Driven by a shared passion for elevating brands, we HiveMinders understands our clients' unique goals and aspirations. We leverage our expertise to craft compelling brand experiences, ensuring that every campaign is executed with precision, creativity, and an unwavering commitment to excellence. <br/><br/> Driven by a shared passion for elevating brands, we HiveMinders understands our clients' unique goals and aspirations. We leverage our expertise to craft compelling brand experiences, ensuring that every campaign.",
-    list: [
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor"
-
-    ]
+    title1: " FMCG & ",
+    title2: "Beauty",
+    para: "In the fast-paced FMCG and beauty sector, HiveMinds delivers tailored digital strategies that drive consumer engagement and brand loyalty. Leveraging deep market insights, creative content, and data-driven tactics, we empower numerous leading brands to optimize visibility, foster growth, and thrive globally in a competitive marketplace.",
+   
   },
   {
     img: "/assets/images/industry/industry-listing.png",
-    title1: " Health &",
+    title1: " Health and",
     title2: "Wellness",
-     para: "Driven by a shared passion for elevating brands, we HiveMinders understands our clients' unique goals and aspirations. We leverage our expertise to craft compelling brand experiences, ensuring that every campaign is executed with precision, creativity, and an unwavering commitment to excellence. <br/><br/> Driven by a shared passion for elevating brands, we HiveMinders understands our clients' unique goals and aspirations. We leverage our expertise to craft compelling brand experiences, ensuring that every campaign.",
-    list: [
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor"
-
-    ]
+    para: "We pioneer digital transformation in the health and wellness industry, delivering innovative, data-backed solutions for brands. By merging creative strategy with analytical rigor, we boost brand visibility, enhance patient engagement, and drive measurable, sustainable growth for lasting success remarkably.",
+   
   },
   {
     img: "/assets/images/industry/industry-listing.png",
     title1: " BFSI -",
     title2: "Fintech",
-     para: "Driven by a shared passion for elevating brands, we HiveMinders understands our clients' unique goals and aspirations. We leverage our expertise to craft compelling brand experiences, ensuring that every campaign is executed with precision, creativity, and an unwavering commitment to excellence. <br/><br/> Driven by a shared passion for elevating brands, we HiveMinders understands our clients' unique goals and aspirations. We leverage our expertise to craft compelling brand experiences, ensuring that every campaign.",
-    list: [
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor"
-
-    ]
+    para: "For BSFI-Fintech brands, we offer full-funnel digital marketing solutions that power growth at every stage. By merging data-driven strategies with innovative creative execution, we optimize customer journeys, boost product adoption, and secure long-term success in a rapidly evolving digital ecosystem.",
+   
   },
   {
     img: "/assets/images/industry/industry-listing.png",
-    title1: " FMCG &",
-    title2: "Beauty",
-     para: "Driven by a shared passion for elevating brands, we HiveMinders understands our clients' unique goals and aspirations. We leverage our expertise to craft compelling brand experiences, ensuring that every campaign is executed with precision, creativity, and an unwavering commitment to excellence. <br/><br/> Driven by a shared passion for elevating brands, we HiveMinders understands our clients' unique goals and aspirations. We leverage our expertise to craft compelling brand experiences, ensuring that every campaign.",
-    list: [
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor"
-
-    ]
+    title1: " E-",
+    title2: "commerce",
+    para: "In the competitive e-commerce landscape, we drive digital growth through paid and organic strategies. Our data-driven, creative approach optimizes customer journeys, boosts conversions, and expands market share for leading brands, delivering measurable success and sustainable revenue growth.",
+   
   },
   {
     img: "/assets/images/industry/industry-listing.png",
     title1: " Fashion & ",
     title2: " Lifestyle",
-     para: "Driven by a shared passion for elevating brands, we HiveMinders understands our clients' unique goals and aspirations. We leverage our expertise to craft compelling brand experiences, ensuring that every campaign is executed with precision, creativity, and an unwavering commitment to excellence. <br/><br/> Driven by a shared passion for elevating brands, we HiveMinders understands our clients' unique goals and aspirations. We leverage our expertise to craft compelling brand experiences, ensuring that every campaign.",
-    list: [
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor"
-
-    ]
+    para: "In the ever-evolving world of fashion and lifestyle, we craft cutting-edge digital campaigns through full-spectrum programmatic ads and marketplace activations. Our strategies amplify brand appeal, spark consumer engagement, and deliver measurable growth, positioning your brand as a leader in style and innovation",
+   
   },
   {
     img: "/assets/images/industry/industry-listing.png",
-    title1: " Electronics &",
-    title2: "Consumer Durables",
-     para: "Driven by a shared passion for elevating brands, we HiveMinders understands our clients' unique goals and aspirations. We leverage our expertise to craft compelling brand experiences, ensuring that every campaign is executed with precision, creativity, and an unwavering commitment to excellence. <br/><br/> Driven by a shared passion for elevating brands, we HiveMinders understands our clients' unique goals and aspirations. We leverage our expertise to craft compelling brand experiences, ensuring that every campaign.",
-    list: [
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor"
-
-    ]
+    title1: " Electronics",
+    title2: "",
+    para: "For electronics brands, HiveMinds delivers dynamic digital strategies that optimize every touchpoint—from discovery to conversion. Our advanced analytics and creative prowess drive superior engagement, ensuring brands stand out and achieve robust growth in the fast-paced electronics market.",
+   
   }, {
     img: "/assets/images/industry/industry-listing.png",
-    title1: " Telecom &",
-    title2: " Automobiles",
-     para: "Driven by a shared passion for elevating brands, we HiveMinders understands our clients' unique goals and aspirations. We leverage our expertise to craft compelling brand experiences, ensuring that every campaign is executed with precision, creativity, and an unwavering commitment to excellence. <br/><br/> Driven by a shared passion for elevating brands, we HiveMinders understands our clients' unique goals and aspirations. We leverage our expertise to craft compelling brand experiences, ensuring that every campaign.",
-    list: [
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor"
-
-    ]
+    title1: " Consumer ",
+    title2: " Durables",
+    para: "In the consumer durables sector, we fuse creative innovation with data-driven strategies to build lasting brand impact. Our tailored campaigns enhance customer experiences, boost conversions, and foster loyalty, positioning brands for sustainable growth in a competitive marketplace.",
   },
   {
     img: "/assets/images/industry/industry-listing.png",
-    title1: " Travel &",
+    title1: " Telecom",
+    title2: " ",
+    para: "We transform telecom marketing by integrating innovative performance strategies, targeted paid advertising, and dynamic social media campaigns. Our solutions boost customer acquisition and engagement, securing a dominant digital presence and driving superior ROI for telecom brands.",
+  },
+  {
+    img: "/assets/images/industry/industry-listing.png",
+    title1: " Automobiles",
+    title2: " ",
+    para: "In the automobile sector, we fuel brand performance with strategic paid advertising, performance marketing, and impactful social media boosting. Our tailored campaigns drive consumer engagement and conversions, enabling automotive brands to accelerate growth anddominate the digital arena.",
+  },
+  {
+    img: "/assets/images/industry/industry-listing.png",
+    title1: " Travel and",
     title2: " Tourism",
-     para: "Driven by a shared passion for elevating brands, we HiveMinders understands our clients' unique goals and aspirations. We leverage our expertise to craft compelling brand experiences, ensuring that every campaign is executed with precision, creativity, and an unwavering commitment to excellence. <br/><br/> Driven by a shared passion for elevating brands, we HiveMinders understands our clients' unique goals and aspirations. We leverage our expertise to craft compelling brand experiences, ensuring that every campaign.",
-    list: [
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor",
-      "Lorem Ipsum Dolor"
-
-    ]
+    para: "We transform travel brands into digital destinations. Our integrated strategies blend data-driven insights, creative storytelling, and performance optimization to elevate online visibility, drive bookings, and create memorable journeys that resonate with globetrotters.",
   }
 ]
