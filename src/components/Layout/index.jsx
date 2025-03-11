2719be51082c7bbdcacbe131e2868379f5e92504
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2025-03-11 10:58:02 +0530

    Update

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -6,26 +6,25 @@ import FooterMobile from "../FooterMobile";
 import Header from "../Header";
 import Loader from "../Loader";
 
-const Layout = ({ children ,isOpen}) => {
-    const [mobileWidth, setMobileWidth] = useState(false);
-        useEffect(() => {
-          if (globalThis.innerWidth > 541) {
-            setMobileWidth(false);
-          } else {
-            setMobileWidth(true);
-          }
-        }, []);
-    return (
-        <>
-           
-            <Header isOpen={isOpen} />
-            <main className="relative z-[1]">
-                {children}
-            </main>
-            {!mobileWidth&&<Footer />}
-           {mobileWidth&&<FooterMobile/>}
-        </>
-    )
+const Layout = ({ children, isOpen }) => {
+  const [mobileWidth, setMobileWidth] = useState(false);
+  useEffect(() => {
+    if (globalThis.innerWidth > 541) {
+      setMobileWidth(false);
+    } else {
+      setMobileWidth(true);
+    }
+  }, []);
+  return (
+    <>
+      <Header isOpen={isOpen} />
+      <main className="relative z-[1]">
+        {children}
+      </main>
+      {!mobileWidth && <Footer />}
+      {mobileWidth && <FooterMobile />}
+    </>
+  )
 }
 
 export default Layout;
