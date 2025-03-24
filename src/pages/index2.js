ee12d89e1029fc959d7d0164bd6e25de7fcc85e7
Harshgoyal-WebDev <138307903+Harshgoyal-WebDev@users.noreply.github.com>
2025-03-24 13:00:45 +0530

    Create index2.js

---
+++ src/pages/index2.js
@@ -0,0 +1,93 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import Layout from '@/components/Layout'
+import Hero from '@/components/Homepage/Hero'
+import Contact from '@/components/Homepage/Contact'
+import Awards from '@/components/Homepage/Awards'
+import Story from '@/components/Homepage/Story'
+import TeamMembers from '@/components/Homepage/TeamMembers'
+import { fadeIn, fadeUp, headingAnim, paraAnim } from '@/components/gsapAnimations'
+import React, { useEffect, useState } from 'react'
+import { Clients2 } from '@/components/Homepage/Clients2'
+import Services from '@/components/Homepage/Solutions'
+import MobileHero from '@/components/Homepage/MobileHero'
+import Metadata from '@/components/Metadata'
+import { getHomePageCaseStudies } from '@/lib/casestudies'
+import { getAllIndustries } from '@/lib/industries'
+import { WebpageJsonLd } from '@/lib/json-ld'
+import { getAllPosts } from '@/lib/blogs'
+import LoaderHome from '../components/Loader'
+import dynamic from 'next/dynamic'
+
+
+const BlogComponent = dynamic(() => import("@/components/Homepage/Blogs"));
+const CaseStudyComponent = dynamic(()=>import("@/components/Homepage/CaseStudies"))
+const metadata = {
+  title: "HiveMinds | Data-Driven Digital Marketing for Business Growth",
+  metaDescription: "HiveMinds delivers strategic, data-driven digital marketing solutions to help startups and brands grow sustainably with advanced technology and expertise.",
+  path: "",
+  img: "homepage.png",
+  date_published: "2025-03-21T00:00",
+  date_modified: "2025-03-21T00:00",
+}
+const index = ({ stickyCaseStudies, filteredPosts }) => {
+
+  const [mobileWidth, setMobileWidth] = useState(false);
+  useEffect(() => {
+    if (globalThis.innerWidth > 1024) {
+      setMobileWidth(false);
+    } else {
+      setMobileWidth(true);
+    }
+  }, []);
+  const [isOpen, setIsOpen] = useState(false);
+
+  paraAnim();
+  headingAnim()
+  fadeUp();
+  fadeIn();
+
+
+  return (
+    <>
+      <Metadata metadata={metadata} />
+      <WebpageJsonLd metadata={metadata} />
+      <LoaderHome />
+      <Layout isOpen={isOpen}>
+        {/* {!mobileWidth ? <Hero /> : <MobileHero />} */}
+     
+        <Story isOpen={isOpen} setIsOpen={setIsOpen} />
+        <Services />
+        <Clients2 />
+        <TeamMembers />
+        {/* <div className='relative h-fit w-screen rounded-[20px]  '>
+          <span className=' block absolute h-[20%] w-full'></span>
+          <CaseStudyComponent caseStudies={stickyCaseStudies} />
+          <Awards />
+          <span className='absolute h-[20%] bottom-[-10%]  w-full'></span>
+          <BlogComponent posts={filteredPosts} />
+        </div> */}
+        <Contact title1={"Looking to Drive "} title2={"Growth?"} para={"We're passionate about delivering results and addressing the challenges that matter most to your business. To learn more, get in touch with us."} />
+      </Layout>
+    </>
+  )
+}
+
+export default index;
+
+export async function getStaticProps() {
+  const { caseStudies } = await getHomePageCaseStudies();
+  const { industries } = await getAllIndustries();
+  const { posts } = await getAllPosts()
+  const filteredPosts = posts.filter((post) => post?.blogFields.blogType[0] == "blog" || post?.blogFields.blogType[0] == "Blog")
+
+  const stickyCaseStudies = caseStudies.filter((caseStudy) => caseStudy.caseStudyFields.ishomepagepost) || null;
+  return {
+    props: {
+      posts,
+      filteredPosts,
+      stickyCaseStudies,
+      industries,
+    },
+    revalidate: 500,
+  };
+}
